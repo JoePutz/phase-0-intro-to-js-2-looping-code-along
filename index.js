c1d3da8579d44c1f66c22488f7de3cce8351b7b0
Joseph <josephputz89@gmail.com>
2022-05-17 09:51:36 -0500

    commit tree 168e68845d97828c1e02fef0dd8421096fd9f39e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const cards = [];
+    const newNames = [...names];
+    for (let i = 0; i < newNames.length; i++) {
+        cards.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+
+    }
+}
